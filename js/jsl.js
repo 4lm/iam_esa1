ec12e555bd07ba43a5099ee87a9b3b1f4fd55dfb
Alexis Michaltsis <alexis@michaltsis.net>
2019-04-26 18:27:16 +0200

    Add list item selection

--- js/jsl.js
+++ js/jsl.js
@@ -2,23 +2,34 @@
  * Created by master on 01.03.16.
  */
 function initialiseView() {
-    var header = document.getElementsByTagName("header")[0];
-    var main = document.querySelector("main");
-    var refresh = document.querySelector("footer .refresh-button")
+    const header = document.getElementsByTagName("header")[0];
+    const main = document.querySelector("main");
+    const refresh = document.querySelector("footer .refresh-button")
 
+    // Switching views
     header.onclick = () => {
         document.body.classList.toggle("tiles");
     }
 
+    // Fade in|out
     refresh.onclick = () => {
         main.classList.toggle("faded");
         main.addEventListener("transitionend", ontransitionend);
     }
 
-    function ontransitionend() {
+    ontransitionend = () => {
         main.classList.toggle("faded");
         main.removeEventListener("transitionend", ontransitionend);
     }
+
+    // List item selection
+    main.querySelectorAll("li").forEach(li => {
+        li.onclick = (e) => {
+            alert("Select: " + getLiTitle(li) + ", click on: " + e.target)
+        }
+    });
+
+    getLiTitle = (li) =>li.getElementsByTagName("h2")[0].textContent;
 }
 
 window.onload = initialiseView;
