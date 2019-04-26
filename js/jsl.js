6c320fad0a04f8beee24aa7816848b9f32e027c9
Alexis Michaltsis <alexis@michaltsis.net>
2019-04-26 19:07:34 +0200

    Simplify list item selection with 1 event listener

--- js/jsl.js
+++ js/jsl.js
@@ -1,10 +1,12 @@
+"use strict;"
 /**
  * Created by master on 01.03.16.
  */
-function initialiseView() {
+initialiseView = () => {
     const header = document.getElementsByTagName("header")[0];
     const main = document.querySelector("main");
-    const refresh = document.querySelector("footer .refresh-button")
+    const refresh = document.querySelector("footer .refresh-button");
+    const ul = main.getElementsByTagName("ul")[0];
 
     // Switching views
     header.onclick = () => {
@@ -23,13 +25,24 @@ function initialiseView() {
     }
 
     // List item selection
-    main.querySelectorAll("li").forEach(li => {
-        li.onclick = (e) => {
-            alert("Select: " + getLiTitle(li) + ", click on: " + e.target)
-        }
-    });
+    ul.onclick = (e) => {
+        const li = getCurrentLi(e.target);
+        alert("Selected: " + getLiTitle(li) + " Click event on: " + e.target);
+    }
 
-    getLiTitle = (li) =>li.getElementsByTagName("h2")[0].textContent;
+    getCurrentLi = (el) => {
+        if (el.tagName === "LI") {
+            return el;
+        } else if (el.tagName === "UL") {
+            alert("Reached list root, no LI found.");
+        } else if (el.parentNode){
+            return getCurrentLi(el.parentNode);
+        } else {
+            alert("Something went completly wrong.");
+        }
+    }
+
+    getLiTitle = (li) => li.getElementsByTagName("h2")[0].textContent;
 }
 
 window.onload = initialiseView;
