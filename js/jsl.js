03d6ed60e1b0b56ac0891e0eca891a4ccf30fd21
Alexis Michaltsis <alexis@michaltsis.net>
2019-04-26 13:59:19 +0200

    Add toggle listeners

--- js/jsl.js
+++ js/jsl.js
@@ -1,3 +1,18 @@
 /**
  * Created by master on 01.03.16.
  */
+function initialiseView() {
+    var header = document.getElementsByTagName("header")[0];
+    var main = document.querySelector("main");
+    var refresh = document.querySelector("footer .refresh-button")
+
+    header.onclick = () => {
+        document.body.classList.toggle("tiles");
+    }
+
+    refresh.onclick = () => {
+        main.classList.toggle("faded");
+    }
+}
+
+window.onload = initialiseView;
