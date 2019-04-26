cfe1d71a9d73310b472af3ff7f5e300d7940aaee
Alexis Michaltsis <alexis@michaltsis.net>
2019-04-26 19:38:57 +0200

    Add new elements alert

--- js/jsl.js
+++ js/jsl.js
@@ -2,11 +2,12 @@
 /**
  * Created by master on 01.03.16.
  */
-initialiseView = () => {
+function initialiseView() {
     const header = document.getElementsByTagName("header")[0];
     const main = document.querySelector("main");
     const refresh = document.querySelector("footer .refresh-button");
     const ul = main.getElementsByTagName("ul")[0];
+    const add = header.querySelector(".add-button");
 
     // Switching views
     header.onclick = () => {
@@ -19,9 +20,9 @@ initialiseView = () => {
         main.addEventListener("transitionend", ontransitionend);
     }
 
-    ontransitionend = () => {
-        main.classList.toggle("faded");
+    function ontransitionend() {
         main.removeEventListener("transitionend", ontransitionend);
+        main.classList.toggle("faded");
     }
 
     // List item selection
@@ -30,7 +31,7 @@ initialiseView = () => {
         alert("Selected: " + getLiTitle(li) + " Click event on: " + e.target);
     }
 
-    getCurrentLi = (el) => {
+    function getCurrentLi(el) {
         if (el.tagName === "LI") {
             return el;
         } else if (el.tagName === "UL") {
@@ -42,7 +43,15 @@ initialiseView = () => {
         }
     }
 
-    getLiTitle = (li) => li.getElementsByTagName("h2")[0].textContent;
+    function getLiTitle(li) {
+        li.getElementsByTagName("h2")[0].textContent;
+    }
+
+    // Add new elements
+    add.onclick = (e) => {
+        e.stopPropagation();
+        alert("add new element");
+    }
 }
 
 window.onload = initialiseView;
